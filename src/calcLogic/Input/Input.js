d19d1ab9a779743cecec52a200ff43a71776410c
Maltex <henrik,nilsson88@gmail.com>
2019-06-10 11:48:54 +0200

    changes

--- src/calcLogic/Input/Input.js
+++ src/calcLogic/Input/Input.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import './Input.css'
 const InputField = (props) => {
 
     return (
       <input
+        className="calcDisplay"
         type="text"
         value={props.value}
         onChange={props.onChange} />
--- src/calcLogic/calcLogic.js
+++ src/calcLogic/calcLogic.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './calcLogic.css'
 import InputField from './Input/Input'
-import Button from './Button/Button'
+
 
 class CalcLogic extends React.Component {
   constructor(props) {
@@ -10,7 +10,8 @@ class CalcLogic extends React.Component {
       currentValue: null,
       displayValue: 0,
       savedValue: null,
-      selectedOperand: "",
+      firstselectedOperand: "",
+      secondselectedOperand: "",
       numberOfCalculations: 0,
       listOfCalculations: [
 
@@ -18,10 +19,7 @@ class CalcLogic extends React.Component {
       completedCalculations: [
 
       ],
-      operandbuttons: [
-        <button value={'+'} >+</button>,
-        <button value={'-'} >-</button>,
-      ],
+
 
     }
   }
@@ -32,127 +30,225 @@ class CalcLogic extends React.Component {
   }
 
   operandSelected = (e) => {
-    let operandSelected = e.target.value;
-    let currentValue = this.state.displayValue;
+    let value
+    let addCalculationsList = [...this.state.listOfCalculations];
 
-    if (operandSelected === "+") {
-      if (currentValue !== null) {
-        this.setState(prevState => ({
-          currentValue: prevState.currentValue + +currentValue,
+    if (this.state.currentValue !== null) {
+
+      if (this.state.firstselectedOperand !== "") {
+        if (this.state.firstselectedOperand === "+") {
+           value = +this.state.currentValue + +this.state.displayValue
+        }
+        if (this.state.firstselectedOperand === "-") {
+           value = +this.state.currentValue - +this.state.displayValue
+        }
+        addCalculationsList.push([this.state.displayValue, " ", e.target.value, " ",])
+        this.setState({
+          listOfCalculations: addCalculationsList,
+          currentValue: value,
+          firstselectedOperand: e.target.value,
           displayValue: 0,
-          numberOfCalculations: prevState.numberOfCalculations + +1
-        }))
+        })
       }
+
     } else {
-      this.setState(prevState => ({
-        currentValue: prevState.currentValue - +currentValue,
+      addCalculationsList.push([this.state.displayValue, " ", e.target.value, " "])
+      this.setState({
+        listOfCalculations: addCalculationsList,
+        currentValue: this.state.displayValue,
+        firstselectedOperand: e.target.value,
         displayValue: 0,
-        numberOfCalculations: prevState.numberOfCalculations + +1
-      }))
+      })
     }
-    this.setState({
-      selectedOperand: operandSelected,
-    })
-    console.log(this.state.numberOfCalculations)
+
+
   }
 
   calculateValues = () => {
-    let addCalculationsList = [...this.state.completedCalculations];
-    let currentValue = this.state.displayValue;
-    let newValue;
-    let selectedOperand = this.state.selectedOperand;
+    let currentValue = +this.state.currentValue;
+    let displayValue = +this.state.displayValue;
+    let firstselectedOperand = this.state.firstselectedOperand;
+    let addCalculationsList = [...this.state.listOfCalculations];
+    let completedCalculations = [...this.state.completedCalculations];
+    let value;
+    if (this.state.currentValue !== null) {
 
-    if (currentValue !== 0) {
-      newValue = +this.state.displayValue + +this.state.currentValue;
+      if (firstselectedOperand === "+") {
+        value = currentValue + displayValue
+      }
+      if (firstselectedOperand === "-") {
+        value = currentValue - displayValue
+      }
 
-      this.setState({
-        currentValue:  newValue,
-      })
-      addCalculationsList.push([newValue, " "])
-
-      this.setState({
-        completedCalculations: addCalculationsList,
-        selectedOperand: "",
+      addCalculationsList.push([displayValue, " = ", value])
+      completedCalculations.push(<li key={this.state.numberOfCalculations}>{addCalculationsList}</li>)
+      this.setState(prevState => ({
+        numberOfCalculations:  prevState.numberOfCalculations  +1,
         currentValue: null,
         displayValue: 0,
-        numberOfCalculations: 0,
-      })
-    } else {
-      addCalculationsList.push([this.state.currentValue, " "])
-      this.setState({
-        completedCalculations: addCalculationsList,
-        selectedOperand: "",
-        currentValue: null,
-        displayValue: 0,
-        numberOfCalculations: 0,
-      })
+        listOfCalculations: [],
+        completedCalculations: completedCalculations,
+        firstselectedOperand: ""
+       }))
+
+
+
+      console.log('push', this.state.completedCalculations)
     }
-  }
 
-  resetValue = () => {
-    this.setState({
-      displayValue: 0,
-      currentValue: 0,
-      numberOfCalculations: 0,
-    })
   }
 
   saveState = (e) => {
     if (e.target.value === 'M+') {
       this.setState({
-        savedValue: this.state.currentValue
+
+        savedValue: this.state.currentValue,
+
       })
     } else {
       this.setState({
-        displayValue: this.state.savedValue
+
+        displayValue: this.state.savedValue,
       })
     }
   }
+  resetValue = () => {
+    this.setState({
+      displayValue: 0,
+      listOfCalculations: [],
+      currentValue: null,
+      firstselectedOperand: "",
+
+    })
+  }
 
   render() {
-    const btnlist = this.state.operandbuttons;
-    let calcList = this.state.listOfCalculations;
-    let showResults;
     let savedValue;
-    //create list of buttons
-    const listitem = btnlist.map((btn, index) =>
-    <li
-    key={btn.key + index}
-    value={btn.value}
-    onClick={e => this.operandSelected(e, btn.value)}
-    >{btn}</li>
-    )
-    //render based on situation
-    if (this.state.numberOfCalculations > 1) {
-      showResults = <p> OnGoing Calculation value:{this.state.currentValue}</p>
-    } else {
-      showResults = <p> OnGoing Calculation value: </p>
-    }
+    let displayValue
+
     if(this.state.savedValue) {
       savedValue = <p>Your saved value is: {this.state.savedValue}</p>
     } else {
       savedValue = <p>You dont have a saved value, use M+ to save your current value</p>
     }
 
+    if (this.state.displayValue === 0){
+      displayValue = ""
+    } else {
+      displayValue = this.state.displayValue
+    }
+
     return (
-      <div>
+      <div className="mainCalculator">
         <InputField
+          className="calcDisplay"
           value={this.state.displayValue}
           onChange={this.handleChanges}/>
 
         <div className="listOfButtons">
-          <Button value={listitem}/>
+          <button onClick={this.operandSelected} value={'+'}>+</button>
+          <button onClick={this.operandSelected} value={'-'}>-</button>
           <button onClick={this.calculateValues}value={'='}>=</button>
           <button onClick={this.resetValue} value={'CE'}>CE</button>
           <button  onClick={this.saveState} value={'M+'} >M+</button>
           <button  onClick={this.saveState} value={'M-'} >M-</button>
         </div>
-        {showResults}
-        {savedValue}
-        <p>list of complete calculations: {this.state.completedCalculations} </p>
+          <p>Current Value: {this.state.currentValue} {this.state.firstselectedOperand} {displayValue}</p>
+          <p> Completed Calculations: {this.state.completedCalculations}</p>
+          {savedValue}
+          <p>Ongoing Calculation: {this.state.listOfCalculations} </p>
       </div>
     )
   }
 }
 
 export default CalcLogic;
+
+
+// {showResults}
+// {savedValue}
+//
+
+
+
+
+
+
+
+
+
+//
+// let operandSelected = e.target.value;
+// let currentValue = this.state.displayValue;
+//
+// if (operandSelected === "+") {
+//   if (currentValue !== null) {
+//     this.setState(prevState => ({
+//       currentValue: prevState.currentValue + +currentValue,
+//       displayValue: 0,
+//       numberOfCalculations: prevState.numberOfCalculations + +1
+//     }))
+//
+//   }
+// } else {
+//
+//   if (currentValue !== null) {
+//     if (this.state.numberOfCalculations === 0) {
+//       this.setState(prevState => ({
+//         currentValue: +currentValue,
+//         displayValue: 0,
+//         numberOfCalculations: prevState.numberOfCalculations + +1
+//       }))
+//
+//     }
+//     else {
+//       this.setState(prevState => ({
+//         currentValue: prevState.currentValue - +currentValue,
+//         displayValue: 0,
+//         numberOfCalculations: prevState.numberOfCalculations + +1
+//       }))
+//     }
+//   }
+// }
+//
+// this.setState({
+//   selectedOperand: operandSelected,
+// })
+
+
+// // if (currentValue !== 0) {
+// //   if (selectedOperand === '-') {
+// //     newValue = this.state.currentValue - this.state.displayValue ;
+// //   }
+// //   if (selectedOperand === '+') {
+// //     newValue = +this.state.displayValue + +this.state.currentValue;
+// //   }
+// let addCalculationsList = [...this.state.completedCalculations];
+// let currentValue = this.state.displayValue;
+// let newValue;
+// let selectedOperand = this.state.selectedOperand;
+//
+//
+// /*this.setState({
+// currentValue:  newValue,
+// })*/
+// addCalculationsList.push([newValue, " "])
+//
+// } else {
+//   addCalculationsList.push([this.state.currentValue, " "])
+// }
+// this.setState({
+//   completedCalculations: addCalculationsList,
+//   selectedOperand: "",
+//   currentValue: null,
+//   displayValue: 0,
+//   numberOfCalculations: 0,
+// })
+// }
+//
+// resetValue = () => {
+//   this.setState({
+//     displayValue: 0,
+//     currentValue: 0,
+//     numberOfCalculations: 0,
+//   })
